5b33705d9ffc7a6b571f6151bcfe1f165283bee7
GiltMonster <lucassantoslukas2@gmail.com>
2024-06-12 19:51:15 -0300

    Feat: tela inicial adicionada.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  userQuestion: string = '';
+  userQuery: string = '';
+  answer: string = 'teste para ver se funciona';
 
 }
